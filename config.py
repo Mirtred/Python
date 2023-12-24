bba7667ada9c2c38e16d6132ba9d4266ffd69329
mirto <mirtoleg1991@gmail.com>
2023-12-24 19:13:57 +0300

    Not work project

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from trello import TrelloClient
 from aiogram import Bot, Dispatcher
 
 
-TOKEN = '6757291411:AAE66i_EJvp0FPFIzMRmvGIIOMRImYFQWCg'
-ADMIN = 851766492
+TOKEN = ''
+ADMIN =
 
 
 FAQ_URL_1 = "facebook.com"
@@ -17,9 +17,9 @@ FAQ_URL_8 = ""
 FAQ_URL_9 = ""
 
 
-TRELLO_API_KEY = '7becfc205a732ce29804b66f6c0d0493'
-TRELLO_TOKEN = 'ATTA6144f2c536973c50aa8ba2a183c1fdf6180fff0b352866c3d27fdbc6d7b6a2220A3829BF'
-TRELLO_BOARD_ID = 'KGj8jeNn'
+TRELLO_API_KEY = ''
+TRELLO_TOKEN = ''
+TRELLO_BOARD_ID = ''
 YOUR_TRELLO_LIST_NAME = "Актуальные Вакансии"
 
 
